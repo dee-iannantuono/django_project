1046e55db2e990a84a9c7fbca515c165a78dd6c6
Dee <deanna.iannantuono@gmail.com>
2023-07-22 10:06:19 +1000

    created a profile page

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,10 +1,11 @@
 # users/urls.py
 from django.urls import path
 from .views import CreateAccountView
-
+from . import views
 
 app_name ='users'
 
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('<int:pk>/accountview', views.NewPageView.as_view(), name='accountview'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -3,8 +3,14 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from django.views.generic import DetailView
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+class NewPageView(DetailView):
+    model = CustomUser
+    template_name = 'users/accountview.html'
+    context_object_name = 'account'
